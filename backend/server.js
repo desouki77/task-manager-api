fbccaf7ff04ba4382a16f7fb1705fa7fef95e644
desouki77 <mahmoud.desouki77@gmail.com>
2024-10-08 18:47:31 +0300

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config(); // Load environment variables
+
+const app = express();
+app.use(express.json()); // Middleware to parse JSON
+
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Base route for testing
+app.get('/' , (req,res) => {
+    res.send('Task Manager API');
+});
+
+app.listen(PORT , ()=> {
+    console.log("Hello Doss");
+    console.log(`Server running on port ${PORT}`);
+});
+
